dee10687b7f06a668e29cf382d2c51c347904fb6
JohnBCasey <caseyjohnb@gmail.com>
2016-06-01 09:47:30 -0400

    Initial Commit

---
+++ AI/Eval.py
@@ -0,0 +1,12 @@
+'''
+Title: Eval.py
+
+Language: Python
+
+Author: John B. Casey <caseyjohnb@gmail.com> <github.com/caseyj>
+
+Description: The purpose of this file is to define the evaluation function
+	used by the agent.py file in order to rank and determine the optimal play 
+	during a given turn.
+
+'''
---
+++ AI/agent.py
@@ -0,0 +1,12 @@
+'''
+Title: agent.py
+
+Language: Python
+
+Author: John B. Casey <caseyjohnb@gmail.com> <github.com/caseyj>
+
+Description: The purpose of this file is to define and run the functions
+	necessary to run and operate a card game playing AI. This file 
+	contains functions which take game state input and make game state
+	output. 
+'''
