d88c77095be5cae22e3be413f4afe0feb73f0bc3
JohnBCasey <caseyjohnb@gmail.com>
2016-06-12 19:09:01 -0400

    initial commit

---
+++ AI/Board.py
@@ -0,0 +1,14 @@
+'''
+Title: Board.py
+
+Language: Python
+
+Author: John B. Casey <caseyjohnb@gmail.com> <github.com/caseyj>
+
+Description: The purpose of this file is to define the functions used to access
+	the current board state and return data relevant to items on the board.
+
+'''
+
+def structure_board():
+	return 0
