0d021d9dbe4405d1b283f34cf32a0c2e7505aaae
JohnBCasey <caseyjohnb@gmail.com>
2016-06-13 19:57:34 -0400

    Added functions associated with the basic board that exists and defined what the board requires to run

--- AI/Board.py
+++ AI/Board.py
@@ -44,8 +44,7 @@ class CARD():
 	defines any immediate use behaviors
 	'''
 	def use_card(self):
-		return None
-
+		pass
 '''
 Defines behaviors and features of cards whom can be targetted
 '''
@@ -127,8 +126,81 @@ class Weapon(Targetable):
 Defines functions and objects associated with heroes
 '''
 class HERO(Targetable):
+	
 	def __init__(self, PWR):
 		self.kind = 'h'
 		self.HP = 30
 		self.EFFECT = PWR
+		self.Weapon = None
+
+	def get_AP(self):
+		if self.Weapon != None:
+			return self.Weapon.get_AP()
+		else:
+			return self.AP
+
+
+'''
+Defines the whole AI copy of the current game in play
+'''
+class Board():
+
+	def __init__(self, EnemyHero, MyHero, Enemy_minions, My_minions, My_hand, Mana_Max, My_Mana_Now, Enemy_Mana_Now):
+		self.ENEMY_HERO = EnemyHero
+		self.MY_HERO = MyHero
+		self.ENEMY_MINIONS =Enemy_minions
+		self.MY_MINIONS = My_minions
+		self.MY_HAND = My_hand
+		self.MANA_MAX = Mana_Max
+		self.MY_MANA_NOW = My_Mana_Now
+		self.ENEMY_MANA_NOW = Enemy_Mana_Now
+
+	'''
+	returns the enemy hero object
+	'''
+	def get_My_Enemy_Hero(self):
+		return self.ENEMY_HERO
+
+	'''
+	returns the list of enemy minions
+	'''
+	def get_My_Enemy_Minions(self):
+		return self.ENEMY_MINIONS
+
+	'''
+	returns the object regarding my hero
+	'''
+	def get_My_Hero(self):
+		return self.MY_HERO
+
+	'''
+	returns the list of my minions
+	'''
+	def get_My_Minions(self):
+		return self.MY_MINIONS
+
+	'''
+	returns the list of cards in my hand
+	'''
+	def get_My_Hand(self):
+		return self.MY_HAND
+
+	'''
+	a void function defining what happens when a card is played from hand to field
+	'''
+	def play_card(self, Card):
+		if Card.get_kind() == 'm':
+			#add the minion to the field
+			self.MY_MINIONS.append(Card)
+		elif Card.get_kind() == 's':
+			pass
+		elif Card.get_kind() == 'w':
+			#give the hero a weapon
+			self.MY_HERO.Weapon = Card
+		#remove the card from the hand
+		self.MY_HAND.remove(Card)
+		#use the card effect
+		Card.use_card()
+
+
 
