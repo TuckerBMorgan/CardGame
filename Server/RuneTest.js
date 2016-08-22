cbf90110de7a10f590371f1127ef103c24571f34
JohnBCasey <caseyjohnb@gmail.com>
2016-08-21 21:57:25 -0400

    Test function for movement has been tested and is functional.

--- Server/RuneTest.js
+++ Server/RuneTest.js
@@ -36,6 +36,32 @@ var GUID_Dictionary = function(GUIDs){
 }
 
 /**
+*Takes in a map and returns the keys of the map
+*
+*Inputs:
+	Map which holds the keys we which to lift
+*Outputs:
+	An array of keys from the Map that we can actually use
+*/
+var grab_keyRing = function(mapps){
+	//keys we wish to return
+	var keyset = [];
+	//the iterator we are concerned with
+	var KeyIterator = mapps.keys();
+	//iterate until null
+	var curVal = KeyIterator.next();
+	var done = curVal.done;
+	while(!done){
+		//add the key to the keyset
+		keyset.push(curVal.value);
+		//increment and now check if we are done incrementing
+		curVal = KeyIterator.next();
+		done = curVal.done;
+	}
+	return keyset;
+}
+
+/**
 *creates a superDictionary of the keys from both dictionaries
 	superDictionary is initialized with 0 as all key values 
 *
@@ -48,20 +74,22 @@ var GUID_Dictionary = function(GUIDs){
 var GUID_Keyshare = function(GUID_DICT_A, GUID_DICT_B){
 	//initialize the big dictionary we wish to produce
 	var superDictionary = new Map();
+	var keyset_A = grab_keyRing(GUID_DICT_A);
+	var keyset_B = grab_keyRing(GUID_DICT_B);
 	//iterate through the first map keys and set each entry to 0 in the super
-	GUID_DICT_A.keys().forEach(function(entry){
+	for(var i = 0; i<keyset_A.length; i++){
 		//if we have already seen it ignore, otherwise:
-		if(!superDictionary.has(entry)){
-			superDictionary.set(entry, 0);
+		if(!superDictionary.has(keyset_A[i])){
+			superDictionary.set(keyset_A[i], 0);
 		}
-	});
+	};
 	//iterate through the second map keys and set each entry to 0 in the super
-	GUID_DICT_B.keys().forEach(function(entry){
+	for(var i = 0; i<keyset_B.length; i++){
 		//if we have already seen it ignore, otherwise:
-		if(!superDictionary.has(entry)){
-				superDictionary.set(entry, 0);
+		if(!superDictionary.has(keyset_B[i])){
+				superDictionary.set(keyset_B[i], 0);
 		}
-	});
+	};
 	//return it
 	return superDictionary;
 }
@@ -123,14 +151,15 @@ var GUID_Compare = function(GUID_setA, GUID_setB){
 	//now that thats done
 	//get all GUIDs and iterate through
 	allKeys = GUID_Keyshare(GUID_Dictionary(GUID_setA), GUID_Dictionary(GUID_setB)).keys();
-	allKeys.forEach(function(entry){
+	
+	for(var i = 0; i<allKeys.length; i++){
 		//get the absolute value  of the difference between the counts from each superDictionary
-		var difference = abs(superDictionary_One.get(entry) - superDictionary_Two.get(entry));
+		var difference = abs(superDictionary_One.get(allKeys[i]) - superDictionary_Two.get(allKeys[i]));
 		//if it equals 1 we will set the GUID_found to the GUID
 		if(difference==1){
 			GUID_found = entry;
 		};
-	});
+	};
 	return GUID_found;
 }
 
@@ -152,3 +181,84 @@ var Card_Movement_Proof= function(Card_Area_1_Before,Card_Area_1_After, Card_Are
 	var Card_Area_2_Movement = GUID_Compare(Card_Area_2_Before, Card_Area_2_After);
 	return card_Area_1_Movement == Card_Area_2_Movement;
 }
+
+/**
+*Takes a controller and returns an integer array of card GUIDs in the grave
+*
+*Inputs:
+	A valid controller which we can pull a list of GUIDs in the grave
+*Output:
+	An integer array of GUIDs in the graveyard
+*/
+var get_grave_GUIDs = function(controller){
+	return controller.graveyard; 
+}
+
+/**
+*Takes a controller and returns an integer array of card GUIDs in the hand
+*
+*Inputs:
+	A valid controller which we can pull a list of GUIDs in the hand
+*Output:
+	An integer array of GUIDs in the hand
+*/
+var get_hand_GUIDs = function(controller){
+	return controller.hand;
+}
+
+/**
+*Takes a controller and returns an integer array of card GUIDs in the field
+*
+*Inputs:
+	A valid controller which we can pull a list of GUIDs in the field
+*Output:
+	An integer array of GUIDs in the field
+*/
+var get_field_GUIDs = function(controller){
+	return controller.inPlay;
+}
+
+
+/**
+*Function which returns true if card movement functions work
+*
+*/
+var card_Move_Test = function(){
+	var cg1b = [1111,2222,3333,4444];
+	var cg1b2 = [2222,3333,4444];
+	var cg2b = [0,1];
+	var cg2b2 = [0,1,1111];
+	var cg12b = [0,1,1111,2222,3333,4444];
+
+
+	//test that our GUID Dictionary works
+	var cg1b_dict_keys = GUID_Dictionary(cg1b);
+	var cg2b_dict_keys = GUID_Dictionary(cg2b);
+	var keys = [];
+	var keyset = cg1b_dict_keys.keys();
+
+	console.log(GUID_Keyshare(cg1b_dict_keys, cg2b_dict_keys));
+	console.log(grab_keyRing(GUID_Keyshare(cg1b_dict_keys, cg2b_dict_keys)).sort(), cg12b);
+	console.log(GUID_Compare(cg1b, cg2b));
+	console.log(Card_Movement_Proof(cg1b,cg1b2, cg2b, cg2b2));
+	return (Card_Movement_Proof(cg1b,cg1b2, cg2b, cg2b2))
+}
+
+card_Move_Test();
+
+/**
+*Boolean returns true if damage between minions has correctly been accounted for
+*
+*Inputs:
+	Minion1_Before->the first minion to enter combat, before action
+	Minion1_After->the first minion to enter combat, after action
+	Minion2_Before->the second minion to enter combat, before action
+	Minion2_After->the second minion to enter combat, after action
+*Outputs:
+	Boolean indicating that card combat has occurred correctly
+*
+*/
+var testDamage = function(){
+
+}
+
